f61b0f5c151b6ad87f6c4141d7c581ea93ad522e
小池　浩晃 <hiroakixiaochi847@gmail.com>
2018-07-10 08:59:52 +0900

    form-確認-書き出し-完了-mamp

---
+++ htdocs/0625/php-1.php
@@ -0,0 +1,44 @@
+<?php
+
+$hash=[
+        'family-name' => '名字（漢字）',
+        'given-name' => '名前（漢字）',
+        'family-name-k' => '名字（カタカナ）',
+        'given-name-k' => '名前（カタカナ）',
+        'example-a' => '性別',
+        'year' => '誕生年',
+        'month'=> '誕生月',
+        'day'=> '誕生日',
+        'post-code' => '郵便番号',
+        'address-level1' => '都道府県・市区町村',
+        'adderss-line1' => '番地・号',
+        'address-line2' => 'ビル・マンション名・部屋番号',
+        'tel-1' => 'ご連絡先電話番号の市外局番',
+        'tel-2' => 'ご連絡先電話番号の市内局番',
+        'tel-3' => 'ご連絡先電話番号の加入者番号',
+        'tel-4' => 'ご連絡先電話番号2の市外局番',
+        'tel-5' => 'ご連絡先電話番号2の市内局番',
+        'tel-6' => 'ご連絡先電話番号2の加入者番号',
+        'email' => 'メールアドレス',
+        'email2' => 'メールアドレス (確認）',
+        'organization' => 'ご職業',
+        'pwd' => 'パスワード',
+        'pwd2' => 'パスワード (確認）',
+        'example-3' => 'サービスのご利用者',
+      ];
+
+$usr_data ='';
+foreach ($_POST as $key => $value){
+    $usr_data .= $value.',';
+echo $hash{$key}.'=>'.$value."<br>\n";
+    
+}
+    
+echo '
+<form action = "php-2.php" method = "post">   
+<input type = "hidden" name = "usrdata" value = "'.$usr_data.'">
+<input type = "submit" value = "送信">
+</form>
+';
+
+?>
---
+++ htdocs/0625/php-2.php
@@ -0,0 +1,53 @@
+<?php
+
+$fp = fopen("data0707.csv", "a");
+flock($fp, LOCK_EX);
+fputs($fp, $_POST{'usrdata'}."\n");
+fclose($fp);
+
+echo '書き出し完了'."<br>\n";
+
+
+
+
+
+
+//echo $_POST{'usrdata'}."<br>\n";
+
+$form_email = 'timf0731gb@docomo.ne.jp';
+
+mb_language("japanese");
+mb_internal_encoding("UTF-8");
+
+
+$to = $form_email;
+$title = '管理者用メール';
+$content = htmlspecialchars($_POST{'usrdata'});
+$header = "From: $form_email\nReply-To: $form_email\n";
+
+if(mb_send_mail($to, $title, $content, $header)){
+    echo "管理者用メールを送信しました";
+}else{
+    echo "メールの送信に失敗しました";
+}
+
+
+
+
+
+//mb_language("japanese");
+//mb_internal_encoding("UTF-8");
+//
+//
+//$to = $_POST{'usrdata_a'};
+//$title = '自動返信用メール';
+//$content = htmlspecialchars($_POST{'usrdata'});
+//$header = "From: $form_email\nReply-To: $form_email\n";
+//
+//if(mb_send_mail($to, $title, $content, $header)){
+//    echo "自動返信用メールを送信しました";
+//}else{
+//    echo "メールの送信に失敗しました";
+//}
+
+?>
